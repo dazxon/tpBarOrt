21f0c78a4a9c025802cc801285a363f4f9e1b006
Agustin Donalisio <donalisioagustin@gmail.com>
2023-04-13 16:00:23 -0300

    fixing data, minor

--- contact.js
+++ contact.js
@@ -4,22 +4,22 @@ function openContactForm() {
       name = prompt("No puede quedar vacio, ingrese su nombre");
     }
   
-    var email = prompt("Ingrese su mail");
-    while (!isValidEmail(email)) {
-      email = prompt("Tiene que ser formato email");
+    var mail = prompt("Ingrese su mail");
+    while (!isValidEmail(mail)) {
+      mail = prompt("Tiene que ser formato email");
     }
   
-    var message = prompt("Ingrese su mensaje");
-    while (!message) {
-      message = prompt("No puede quedar vacio, ingrese su mensaje");
+    var mes = prompt("Ingrese su mensaje");
+    while (!mes) {
+      mes = prompt("No puede quedar vacio, ingrese su mensaje");
     }
   
     // do something with the form data
-    alert(`Gracias ${name.charAt(0).toUpperCase() + name.slice(1)} por contactarnos, recibira su respuesta en la casilla de ${email}`);
+    alert(`Gracias ${name.charAt(0).toUpperCase() + name.slice(1)} por contactarnos, recibira su respuesta en la casilla de ${mail}`);
   
-    function isValidEmail(email) {
+    function isValidEmail(mail) {
       var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return emailRegex.test(email);
+      return emailRegex.test(mail);
     }
   }
   
